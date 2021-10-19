a4fa092f582603abc0a77a18ca3424690a2eb7b4
sia-smd <siavashsamadi@gmail.com>
2021-10-20 00:08:01 +0330

    final

--- siavash(calculator)/js/calculator.js
+++ siavash(calculator)/js/calculator.js
@@ -4,34 +4,41 @@
 
 
 function clicks(X) {
-    
+
     var X;
-    var dis=document.getElementById("display1").value
-    if (dis.length>10){
-        document.getElementById('display1').style.fontSize="20px"
-    } 
-    if (dis==0){
+    var dis = document.getElementById("display1").value
+    if (dis.length > 10) {
+        document.getElementById('display1').style.fontSize = "20px"
+    }
+    if (dis == 0) {
         document.getElementById("display1").value = "";
-        document.getElementById("display1").ariaPlaceholder="0"
-    } 
-    document.getElementById("display1").value=document.getElementById("display1").value+X
-    
+        document.getElementById("display1").ariaPlaceholder = "0"
+    }
+    document.getElementById("display1").value = document.getElementById("display1").value + X
+
 }
 
 function act(Y) {
     var Y;
-    var clr=document.getElementById("display1").value
+    var clr = document.getElementById("display1").value
     switch (Y) {
         case 1:
-            debugger
-            clr=clr.slice(0,clr.length-1)
-            document.getElementById("display1").value=clr
+
+            clr = clr.slice(0, clr.length - 1)
+            document.getElementById("display1").value = clr
             break;
         case 2:
-            document.getElementById('display1').style.fontSize="40px";
+            debugger
+            var his=document.getElementById('display2')
+             var his_opt=his.length
+            for (var i=his_opt;i>=0 ;i--){
+                his.remove(i)
+            }
+            document.getElementById("display2").remove(1)
+
+            document.getElementById('display1').style.fontSize = "40px";
             document.getElementById("display1").value = "";
-            document.getElementById("display1").ariaPlaceholder="0";
-            document.getElementById("display2").innerText = "0"
+            document.getElementById("display1").ariaPlaceholder = "0";
             break;
 
     }
@@ -40,9 +47,14 @@ function act(Y) {
 
 
 function Equal() {
-
+    
     var n = document.getElementById("display1").value
-    document.getElementById("display2").innerText = n
+
+        // document.getElementById("display2").innerText = his + ',,' + n
+        var x = document.getElementById("display2");
+        var option = document.createElement("option");
+        option.text = n;
+        x.add(option);
     document.getElementById("display1").value = eval(n)
 
 }
