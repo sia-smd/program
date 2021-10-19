af8abec974b651e7654897ae889c5023e3dbc9bc
sia-smd <siavashsamadi@gmail.com>
2021-10-19 19:25:22 +0330

    final-1

--- siavash(calculator)/js/calculator.js
+++ siavash(calculator)/js/calculator.js
@@ -4,9 +4,12 @@
 
 
 function clicks(X) {
-    debugger
+    
     var X;
     var dis=document.getElementById("display1").value
+    if (dis.length>10){
+        document.getElementById('display1').style.fontSize="20px"
+    } 
     if (dis==0){
         document.getElementById("display1").value = "";
         document.getElementById("display1").ariaPlaceholder="0"
@@ -17,16 +20,18 @@ function clicks(X) {
 
 function act(Y) {
     var Y;
-
+    var clr=document.getElementById("display1").value
     switch (Y) {
         case 1:
-            document.getElementById("display1").value = "";
-            document.getElementById("display1").ariaPlaceholder="0"
+            debugger
+            clr=clr.slice(0,clr.length-1)
+            document.getElementById("display1").value=clr
             break;
         case 2:
-            
-            document.getElementById("display1").innerText = "";
-            document.getElementById("display2").innerText = ""
+            document.getElementById('display1').style.fontSize="40px";
+            document.getElementById("display1").value = "";
+            document.getElementById("display1").ariaPlaceholder="0";
+            document.getElementById("display2").innerText = "0"
             break;
 
     }
@@ -37,7 +42,7 @@ function act(Y) {
 function Equal() {
 
     var n = document.getElementById("display1").value
-    
+    document.getElementById("display2").innerText = n
     document.getElementById("display1").value = eval(n)
 
 }
